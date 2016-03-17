15dd49181c7149f07520cd55a2a4af936f822bef
pecata89 <pecata_11@yahoo.com>
2016-03-17 13:46:27 +0100

    all

--- js/app.js
+++ js/app.js
@@ -1,76 +1,91 @@
 $(document).on("ready", function () {
 
-    $(".calculateForm").on("keyup", function () {
+    // event handler on change for the form
+    // source: https://api.jquery.com/change/
+    $(".calculateForm").change(function () {
 
+        // we are using an array with the alphabet
+        // instead of getting the values from String.fromCharCode
+        // For me it seems easier so far
+        var alphabet = new Array("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z");
 
-        var alphabet = new Array(" ", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z");
+        // adding encription and decription functionality
+        // source: http://stackoverflow.com/questions/7960208/jquery-if-checkbox-is-checked
+        if ($("#decriptMessage").is(":checked")) {
+            alphabet = alphabet;
+            $("#calculate").text("DECRYPT");
+        } else {
+            alphabet = alphabet.reverse();
+            $("#calculate").text("ENCRYPT");
+        }
 
-
-        // console.log(alphabet.length);
-        var ls = $("#letterStatistics");
+        // registering the forms and some variables that we will need later in the code
+        // making arrays of the forms - message, decrypted/encrypted message, key value
         var messageText = $("#decMessage").text();
         var messageLength = $("#decMessage").val().length;
         var messageArray = $("#decMessage").val();
-
         var keyVal = $("#decKey").val();
-        if (!keyVal) {
-            keyVal = 0;
-        }
 
+        // registering the variables that we gonna need in the script
         var decriptedMessage = "";
         var letter = null;
         var letterIndex = null; // the index of the number in the alphabet
         var decLetterIndex = null; // the index of the number when decripted
         var decLetter = null; // letter from the alphabet
-        var dexLetterIndexFinal = null;
-
 
         // loop through each letter of the message
-        // 
-        for (var i = 0; i < (messageLength); i++) {
-            //console.log(alphabet[i]);
+        for (var i = 0; i < (messageLength) ; i++) {
 
-            // console.log(i + " letter: " + messageArray[i]);
-
-            // Saving the letter, so we could find it in the alphabet array 
+            // Saving the letter that is being typed last from the encript/decript message form
             letter = messageArray[i];
+
             // if the letter is uppercase it will change it to lowercase. This is made so there won't be any issues with the array
             if (letter == letter.toUpperCase()) {
                 letter = letter.toLowerCase(letter);
             }
-            // Finding the letter index by running all the letters thought our alphabet array
 
+            // Finding the letter index by getting the Index of the current typed letter through our alphabet array
             // console.log("Letter: " + letter + "\nLetter Index: " + letterIndex);
-
             letterIndex = alphabet.indexOf(letter);
 
-            decLetterIndex = letterIndex + parseInt(keyVal);
-
-
-            if (decLetterIndex > 25) {
-                decLetterIndex = decLetterIndex % 26;
+            // if the user has not entered key to decript the message this code sets the value of the key to 0
+            if (!keyVal) {
+                keyVal = 0;
             }
 
+            // we are getting the decripted letter index
+            // it is the sum of the index of the letter plus the value of the key
+            decLetterIndex = letterIndex + Number(keyVal);
 
+            // to avoid getting in and out of the alphabet array we check if the index of the dectipted letter is bigger than the array lenght
+            // if yes we need to start from the beginning and calculate how many numbers extra we need to jump from the start
+            // we javascript arithmetic operator
+            // source: http://www.w3schools.com/js/js_operators.asp
+            if (decLetterIndex > 25) {
+                decLetterIndex = (decLetterIndex % 26);
+            }
+
+            // here we check if the entered letter is an empty space
             if (letter == " ") {
+                // if yes - the decripted letter will also be an empty space
                 decLetter = " ";
             } else {
-
+                // if not - we search through the alphabet array for the letter that we need to show as dectipted/encripted by using the index of the decripted letter
                 decLetter = alphabet[decLetterIndex];
                 // decLetter = String.fromCharCode(((letter - 96) - (parseInt(keyVal) - 96) + 25) % 26 + 97);
             }
 
-
+            // we add the dectipted letter that we get
+            // the process is simple
+            // 1. we get the letter that is being typed
+            // 2. search what index it is in the alphabet array
+            // 3. we add the key value to the index of the letter that is typed
+            // 4. when we have the index of the decripted letter we search which letter it is in the array
+            // 5. we post the letter to the decripted message variable
             decriptedMessage += decLetter;
-
-
-            // console.log("Decripted Letter Index: " + decLetterIndex + "\nDecripted Letter: " + decLetter);
-            // console.log(decriptedMessage);
-
         }
 
+        // we post the decripted/encripted message to the decripted/encripted form
         $("#decDecripted").text(decriptedMessage);
-
-
     });
 });
--- js/mandatory1.js
+++ js/mandatory1.js
@@ -1,115 +1,65 @@
 $(document).on("ready", function () {
 
-
-
-
     // event handler on change for the form
     // source: https://api.jquery.com/change/
     $(".calculateForm").change(function () {
 
 
-        // we are using an array with the alphabet
-        // instead of getting the values from String.fromCharCode
-        // For me it seems easier so far
-        var alphabet = new Array("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z");
+        // find the letter from array
+        // we are using this function to avoid going through the process of finding letter indexes and reverse
+        function find(array, value) {
+            for (var i = 0; i < array.length; i++) {
+                if (array[i] == value) { return i; }
+            }
 
-
-        // adding encription and decription functionality
-        // source: http://stackoverflow.com/questions/7960208/jquery-if-checkbox-is-checked
-        if ($("#decriptMessage").is(":checked")) {
-            alphabet = alphabet;
-            $("#calculate").text("DECRIPT");
-        } else {
-            alphabet = alphabet.reverse();
-            $("#calculate").text("ENCRIPT");
+            return -1;
         }
 
+        var alphabet = new Array("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z");
 
-        // console.log(alphabet.length);
-        var messageText = $("#decMessage").text();
+        // registering the forms and some variables that we will need later in the code
+        // making arrays of the forms - message, decrypted/encrypted message, key value
         var messageLength = $("#decMessage").val().length;
         var messageArray = $("#decMessage").val();
         var keyVal = $("#decKey").val();
+        var keyLength = $("#decKey").val().length;
+
+        // registering the variables that we gonna need in the script
+        var decriptedSegment = new Array();
 
 
-        var decriptedMessage = "";
-        var letter = null;
-        var letterIndex = null; // the index of the number in the alphabet
-        var decLetterIndex = null; // the index of the number when decripted
-        var decLetter = null; // letter from the alphabet
+        if (keyLength <= messageLength) {
 
+            if ($("#decriptMessage").is(":checked")) {
+                $("#calculate").text("ENCRYPT");
 
-        // loop through each letter of the message
-        for (var i = 0; i < (messageLength) ; i++) {
-            //console.log(alphabet[i]);
+                for (var i = 0; i < messageLength; i++) {
+                    decriptedSegment[i] = ((find(alphabet, messageArray[i]) + find(alphabet, keyVal[i])) % 26);
+                }
 
-            // console.log(i + " letter: " + messageArray[i]);
+                for (var k = 0; k < decriptedSegment.length; k++) {
+                    decriptedSegment[k] = alphabet[decriptedSegment[k]];
+                }
 
-            // Saving the letter that is being typed last from the encript/decript message form
-            letter = messageArray[i];
-
-            // if the letter is uppercase it will change it to lowercase. This is made so there won't be any issues with the array
-            if (letter == letter.toUpperCase()) {
-                letter = letter.toLowerCase(letter);
-            }
-
-
-            // Finding the letter index by getting the Index of the current typed letter through our alphabet array
-            // console.log("Letter: " + letter + "\nLetter Index: " + letterIndex);
-            letterIndex = alphabet.indexOf(letter);
-
-
-            // if the user has not entered key to decript the message this code sets the value of the key to 0
-            if (!keyVal) {
-                keyVal = 0;
-            }
-
-
-            // we are getting the decripted letter index
-            // it is the sum of the index of the letter plus the value of the key
-            decLetterIndex = letterIndex + Number(keyVal);
-
-
-
-            // to avoid getting in and out of the alphabet array we check if the index of the dectipted letter is bigger than the array lenght
-            // if yes we need to start from the beginning and calculate how many numbers extra we need to jump from the start
-            // we javascript arithmetic operator
-            // source: http://www.w3schools.com/js/js_operators.asp
-            if (decLetterIndex > 25) {
-                decLetterIndex = (decLetterIndex % 26);
-            }
-
-
-
-            // here we check if the entered letter is an empty space
-            if (letter == " ") {
-                // if yes - the decripted letter will also be an empty space
-                decLetter = " ";
             } else {
-                // if not - we search through the alphabet array for the letter that we need to show as dectipted/encripted by using the index of the decripted letter
-                decLetter = alphabet[decLetterIndex];
-                // decLetter = String.fromCharCode(((letter - 96) - (parseInt(keyVal) - 96) + 25) % 26 + 97);
+                $("#calculate").text("DECRYPT");
+
+                for (var i = 0; i < messageLength; i++) {
+                    decriptedSegment[i] = ((find(alphabet, messageArray[i]) - find(alphabet, keyVal[i]) + 26) % 26);
+                }
+                for (var k = 0; k < decriptedSegment.length; k++) {
+                    decriptedSegment[k] = alphabet[decriptedSegment[k]];
+                }
             }
 
+            decriptedSegment = decriptedSegment.join('');
 
-            // we add the dectipted letter that we get
-            // the process is simple
-            // 1. we get the letter that is being typed
-            // 2. search what index it is in the alphabet array
-            // 3. we add the key value to the index of the letter that is typed
-            // 4. when we have the index of the decripted letter we search which letter it is in the array
-            // 5. we post the letter to the decripted message variable
-            decriptedMessage += decLetter;
+            $("#decDecripted").val(decriptedSegment);
 
 
-            // console.log("Decripted Letter Index: " + decLetterIndex + "\nDecripted Letter: " + decLetter);
-            // console.log(decriptedMessage);
-
+        } else {
+            $("#decDecripted").text("YOUR KEY VALUE IS LONGER THAN THE MESSAGE!");
         }
-
-        // we post the decripted/encripted message to the decripted/encripted form
-        $("#decDecripted").text(decriptedMessage);
-
 
     });
 });
